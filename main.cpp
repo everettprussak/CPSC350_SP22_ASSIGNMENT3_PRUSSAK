26728d84379b8679e279ac47270076d169723066
everettprussak <prussak3@aol.com>
2022-03-14 12:49:28 -0700

    Finished Main

--- main.cpp
+++ main.cpp
@@ -41,10 +41,10 @@ The string uses .erase and remove to get rid of the newline characters. */
         cout << "Enter Input File Name: " << endl;
         cin >> infile;
         cout << endl;
-        content = file1.process(infile);
+        content = file1.process(infile); //using FileIO class "process" method to get a string of the file
         rows = file1.getRows();
         columns = file1.getColumns();
-        content.erase(remove(content.begin(), content.end(), '\n'), content.end());
+        content.erase(remove(content.begin(), content.end(), '\n'), content.end()); //Takes away newline characters from string
     }
 
 /* The user wants to use random configuration instead. They are asked for row, columns, and Population Density
@@ -65,20 +65,20 @@ density the user wants.
         cin >> pop;
         cout << endl;
         int cells = rows*columns;
-        alive = cells * pop;
+        alive = cells * pop; //This is the amount of cells that will start alives
         char randomArray[cells];
-        srand(time(NULL));
-        for(int i=0; i<cells; ++i){
+        srand(time(NULL)); //starting for random generator I am using.
+        for(int i=0; i<cells; ++i){ //This for loop creates an array of only '-' prior to placing alive cells
             randomArray[i] = '-';
         }
-        while(count<alive){
+        while(count<alive){ //this loop will place random alive cells 'X' in the 1d array.
             randomNum = rand() % cells;
-            if(randomArray[randomNum]!='X'){
+            if(randomArray[randomNum]!='X'){ //This is for checking to make sure the random number is unique
                     randomArray[randomNum] = 'X';
                     count++;
             }
         }
-        for(int f=0; f<cells; f++){
+        for(int f=0; f<cells; f++){ //creating the content string from the 1d array
             content = content + randomArray[f];
         }
     }
@@ -106,32 +106,32 @@ or the output file that the user specified. If the game is not a infitie game, t
 the exit message is displayed, and the the specified gamemode is destructed.
 */
     if(gamemode=="Classic"||gamemode=="classic"||gamemode=="CLASSIC"){
-        Classic *cla = new Classic(rows,columns);
+        Classic *cla = new Classic(rows,columns); //creating the class type
         for(int i=0; i<rows; ++i){
             for(int y=0; y<columns; ++y){
                 char temp = content[counter];
                 counter++;
-                cla->insertChar(i,y,temp);
+                cla->insertChar(i,y,temp); //inserting from our 1d array, to make a 2d array in the class
             }
         }
         if(output_type!="output" && output_type!="Output" && output_type!="OUTPUT"){
-                cla->printGame();
+                cla->printGame(); //If the user wants output file, then The contents will not print on the terminal
         }
 
-        string gametostring = cla->stringGame();
+        string gametostring = cla->stringGame();  //this is unique to output file method.
         string total_contents;
 
-        while(cla->empty()!=1 && cla->stable()!=1){
+        while(cla->empty()!=1 && cla->stable()!=1){ //The loop will continue until it is empty or stable
             if(output_type=="Pause"||output_type=="PAUSE"||output_type=="pause"){
-                system("read -p 'Press Enter to continue...' var");
-                cla->generation();
-                cla->printGame();
+                system("read -p 'Press Enter to continue...' var"); //makes the user click 'enter' to continue
+                cla->generation(); //new generation is generated from the class
+                cla->printGame(); //genreration is printed to terminal
                 continue;
             }
             else if(output_type=="Delay"||output_type=="delay"||output_type=="DELAY"){
-                sleep(2);
-                cla->generation();
-                cla->printGame();
+                sleep(2); //This will make a 2 second delay until the next line of code
+                cla->generation(); //new generation is generated from the class
+                cla->printGame(); //genreration is printed to terminal
                 continue;
             }
             else if(output_type=="output"||output_type=="Output"||output_type=="OUTPUT"){
@@ -144,8 +144,8 @@ the exit message is displayed, and the the specified gamemode is destructed.
                 continue;
 
             }
-            else{
-                cla->generation();
+            else{ //This is assuming that we are using terminal, and it is continually printed onto the screen
+                cla->generation(); 
                 cla->printGame();
                 continue;
             }
